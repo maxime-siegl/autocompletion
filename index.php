8f852e200cc4849658e2f9a865a05d34e5673bbc
maxime <maxime.siegl@laplateforme.io>
2020-11-02 15:45:41 +0100

    structure

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <title></title>
   </head>
   <body>
-
+    <header></header>
+    <main></main>
+    <footer></footer>
   </body>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
