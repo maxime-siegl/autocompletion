e240c8536423fe323db28ef4110fbb5835e7f4e6
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-02 13:31:33 +0100

    Création du fichier index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"/>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+  <script src="js/data.js" charset="utf-8"></script>
+</html>
