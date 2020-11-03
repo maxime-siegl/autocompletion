f8d6e37d50049857969e8df57d292b3c7c5e6031
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-03 10:52:04 +0100

    Autocompletion d'un tableau avec jquery UI

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <title></title>
+
+
   </head>
   <body>
     <header></header>
@@ -15,4 +17,9 @@
   </body>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+  <script src="js/autocompletion.js" charset="utf-8"></script>
 </html>
