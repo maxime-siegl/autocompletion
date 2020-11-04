d619c1fd1aa886117a90592a9de5669a9c8a6e93
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-04 15:48:37 +0100

    Affichage des résultats de la recherche + liens avec get id

--- recherche.php
+++ recherche.php
@@ -6,6 +6,11 @@ if(isset($_GET['search'])){
   $search = $_GET['search'];
 
   $query = $db->query("SELECT * FROM philosophes WHERE nom LIKE '$search%'");
-  $tableau = $query->fetchAll();
-  var_dump($tableau);
+  //$resultats = $query->fetchAll();
+
+   while ($donnees = $query->fetch()){
+     echo $donnees['nom'] . "<br>";
+     echo substr($donnees['description'], 0, 200) . "..." . "<br>";
+     echo "<a href='element.php?id=". $donnees['id'] ."'> Lien vers l'article </a>" ."<br>";
+  }
 } ?>
