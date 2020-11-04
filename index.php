766a91595c07dff569632265184d4cf04fe9dbd5
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-04 14:54:47 +0100

    CSS jquery ui

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title></title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   </head>
   <body>
     <header>
