0aaae95823e449e2f7d016020eb7793f79b8edd9
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-04 16:21:55 +0100

    margin-top footer / 1eres balises html pour la boucle affichant results

--- recherche.php
+++ recherche.php
@@ -6,11 +6,50 @@ if(isset($_GET['search'])){
   $search = $_GET['search'];
 
   $query = $db->query("SELECT * FROM philosophes WHERE nom LIKE '$search%'");
-  //$resultats = $query->fetchAll();
+  ?>
 
-   while ($donnees = $query->fetch()){
-     echo $donnees['nom'] . "<br>";
-     echo substr($donnees['description'], 0, 200) . "..." . "<br>";
-     echo "<a href='element.php?id=". $donnees['id'] ."'> Lien vers l'article </a>" ."<br>";
-  }
-} ?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <title></title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <main class="main_recherche">
+      <?php
+       while ($donnees = $query->fetch()){ ?>
+         <article >
+           <h2> <?php echo $donnees['nom'] . "<br>"; ?> </h2>
+           <p> <?php echo substr($donnees['description'], 0, 200) . "..." . "<br>"; ?></p>
+           <?php echo "<a href='element.php?id=". $donnees['id'] ."'> Lien vers l'article </a>" ."<br>"; ?>
+         </article>
+      <?php
+    }
+  }else{
+    header('Location:index.php');
+  } ?>
+
+    </main>
+
+    <footer>
+      <p>Auto-complétion, développé par Maxime Siegl & Cécile Wojnowski</p>
+    </footer>
+  </body>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+  <script src="js/autocompletion.js" charset="utf-8"></script>
+</html>
