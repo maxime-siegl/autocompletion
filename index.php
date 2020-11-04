7d5b09b3adeb5b471792ff3b380905aed7308d19
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-04 16:41:23 +0100

    Police Prata h1 index

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
   </head>
   <body>
     <header class="header_index">
