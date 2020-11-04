3c3a6bc32b5b974fdcfbaf149c7346843f73dfd1
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-04 10:04:23 +0100

    Requête sélectionnant les résultats commençant par la valeur de $term

--- charge_db.php
+++ charge_db.php
@@ -1,3 +1,9 @@
 <?php
+include("includes/db.php");
 echo json_encode([$_POST["term"], "b"]);
+// Requête comparant le term avec les données de la bdd
+$term = $_POST["term"];
+
+ $query = $db->prepare("SELECT nom FROM philosophes WHERE nom LIKE '$term%' LIMIT 8 ");
+ $query->execute();
 ?>
