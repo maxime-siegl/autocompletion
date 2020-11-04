627f0dc6c67dbd5a501d170b1ff7954d2b970644
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-04 14:08:58 +0100

    Titre + background img + roboto

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <title></title>
-
-
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <header></header>
+    <header>
+      <h1>Biographies de philosophes</h1>
+    </header>
     <main>
+
       <input type="text" id="philosophes" placeholder="Que recherchez-vous ?">
 
     </main>
