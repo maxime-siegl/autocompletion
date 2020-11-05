0f6559e18ccf9de9054fa0abf0d6a3911acdcf5b
maxime <maxime.siegl@laplateforme.io>
2020-11-05 11:00:52 +0100

    modification du port de la db

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-  $db = new PDO('mysql:host=localhost; port=3308; dbname=autocompletion', 'root', '');
+  $db = new PDO('mysql:host=localhost; port=3306; dbname=autocompletion', 'root', '');
 
   $db->exec("SET CHARACTER SET utf8");
 
