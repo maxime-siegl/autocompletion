226b677b437e3e5ddda20e477578ac73dc52e625
maxime <maxime.siegl@laplateforme.io>
2020-11-06 10:56:04 +0100

    pb changement de id search bar header

--- js/search_header.js
+++ js/search_header.js
@@ -2,7 +2,7 @@ var url = window.location.href;
 console.log(url);
 
 if (url === 'http://localhost/autocompletion/index.php' || url === 'http://localhost/autocompletion/'){
-    $('#philosophes_header').hide();
+    $('.bar_header').hide();
 }else{
-    $('#philosophes_header').show();
+    $('.bar_header').show();
 }
