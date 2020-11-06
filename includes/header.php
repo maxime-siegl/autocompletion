49462d1b90418e3b433ae824cee4d9e17a807b93
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-11-06 11:03:52 +0100

    Lien vers l'index dans le header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header class="header_index">
-    <h1>Biographies de philosophes</h1>
+    <h1><a href="index.php"> Biographies de philosophes </a></h1>
     <form id="form_autocomplete_header" class="form_autocomplete" action="recherche.php" method="get">
         <input type="text" name="search" class="input_index bar_header" id="philosophes_header" placeholder="Que recherchez-vous ?">
     </form>
